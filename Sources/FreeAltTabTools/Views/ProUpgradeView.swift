4c8f1d7a49fe15c6eb5629b0876f4a8c36db33a8
Ky Northstar <Ky@KyNorthstar.me>
2026-06-16 02:26:10 -0600

    Toggle can be large on large-supporting systems

--- Sources/FreeAltTabTools/Views/ProUpgradeView.swift
+++ Sources/FreeAltTabTools/Views/ProUpgradeView.swift
@@ -18,6 +18,25 @@ public struct ProUpgradeView: View {
     
     
     public var body: some View {
+        Form {
+            if #available(macOS 14, *) {
+                proModeToggle
+                    .controlSize(.extraLarge)
+            }
+            else if #available(macOS 11, *) {
+                proModeToggle
+                    .controlSize(.large)
+            }
+            else {
+                proModeToggle
+            }
+            
+            Text("You get to decide whether you want to use Pro mode or not. No restrictions!")
+        }
+    }
+    
+    
+    var proModeToggle: some View {
         Toggle("Pro mode", isOn: Binding{
             switch licenseState {
             case .pro: true
@@ -27,7 +46,6 @@ public struct ProUpgradeView: View {
             licenseState = proMode ? .pro : .free
         })
         .toggleStyle(.switch)
-        // …rest of your fancy view
     }
 }
 
